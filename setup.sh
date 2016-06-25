979b72620483f510021e6ee166a5d83ea5825ef4
Klavs Madsen <ktrance@gmail.com>
2016-06-25 18:20:40 +0200

    added docker-compose

--- setup.sh
+++ setup.sh
@@ -10,5 +10,8 @@ apt-get install linux-image-extra-$(uname -r) -qy
 echo "<============= installing docker =============>"
 apt-get install docker.io -qy
 
+echo "<============= installing docker-compose =============>"
+apt-get install docker-compose -qy
+
 echo "<============= ips =============>"
 ifconfig | grep "inet addr"
